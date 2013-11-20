c918e9feb766a3737170a433a0aee46dfacf0a2c
James Tapsell <jrtapsell@gmail.com>
2013-11-20 22:47:44 +0000

    Code Cleanup

--- js/index.js
+++ js/index.js
@@ -1,49 +1,83 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+	function reset()
+	{
+	
+navigator.app.exitApp();
+	}
+	var locations = [[1,2,3],[4,5,9],[7,8,6]];
+	var stop = false;
+	function kingjulian(index)
+	{
+		if(!stop){
+var blank;	
+for( var i = 0, len = 3; i < len; i++ ) {
+	for( var j = 0, len = 3; j < len; j++ ) {
+    if( locations[i][j] === 9 ) {
+        bx = i + 1;
+        by = j + 1;
+        break;
     }
-};
+}	
+}
+var clicked;	
+for( var i = 0, len = 3; i < len; i++ ) {
+	for( var j = 0, len = 3; j < len; j++ ) {
+    if( locations[i][j] === index ) {
+        cx = i + 1;
+        cy = j + 1;
+        break;
+    }
+}	
+}
+console.log("bx " + bx);
+console.log("by " + by);
+console.log("cx " + cx);
+console.log("cy " + cy);
+if(by == cy + 1 && bx == cx){
+	$( "#move" + index ).animate({
+	left: "+=33%",
+});
+locations[bx - 1][by - 1] = index;
+locations[cx - 1][cy - 1] = 9;
+}
+if(by == cy - 1 && bx == cx){
+	$( "#move" + index ).animate({
+	left: "-=33%",
+});
+locations[bx - 1][by - 1] = index;
+locations[cx - 1][cy - 1] = 9;
+}
+if(by == cy && bx == cx + 1){
+	$( "#move" + index ).animate({
+	top: "+=33%",
+});
+locations[bx - 1][by - 1] = index;
+locations[cx - 1][cy - 1] = 9;
+}
+if(by == cy && bx == cx - 1){
+	$( "#move" + index ).animate({
+	top: "-=33%",
+});
+locations[bx - 1][by - 1] = index;
+locations[cx - 1][cy - 1] = 9;
+}
+if (String(locations) === "1,2,3,4,5,6,7,8,9"){
+	setTimeout(function(){alert("You Win")}, 1000);
+	stop = true;
+	
+}else{
+	console.log(String(locations));
+}
+}
+}
+
+for( var i = 0, len = 3; i < len; i++ ) {
+for( var j = 0, len = 3; j < len; j++ ) {
+	var target = locations[i][j];
+	if(target !== 9){
+	$( "#move" + target ).animate({
+	top: "+=" + i * 33 + "%",
+	left: "+=" + j * 33 + "%",
+});
+}
+}
+}
