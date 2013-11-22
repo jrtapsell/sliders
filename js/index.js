44d221701019f559dc8f5dce0e3b70074ac77394
James Tapsell <jrtapsell@gmail.com>
2013-11-22 23:16:19 +0000

    Beutified

--- js/index.js
+++ js/index.js
@@ -14,140 +14,145 @@ var rows = 4;
 var win = "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,42";
 var stop = false;
 $(".WIN").css("display", "none");
-function reset()
-{	
-stop = false;
-$(".WIN").css("display", "none");
-locations = $.parseJSON(raw)[puzzle];
-for( var i = 0, len = rows; i < len; i++ ) {
-for( var j = 0, len = rows; j < len; j++ ) {
-	var target = locations[i][j];
-	if(target !== 42){
-	$( "#move" + target ).animate({
-	top: "" + (i *  (100 / rows)) + "%",
-	left: "" + (j * (100 / rows)) + "%",
-});
-}
-}
-}
-}	
-function next(){
-	if(puzzle !== puzzles){
-	puzzle = puzzle + 1;
-	locations = $.parseJSON(raw)[puzzle];
-	draw();
+
+function reset() {
 	stop = false;
 	$(".WIN").css("display", "none");
+	locations = $.parseJSON(raw)[puzzle];
+	for (var i = 0, len = rows; i < len; i++) {
+		for (var j = 0, len = rows; j < len; j++) {
+			var target = locations[i][j];
+			if (target !== 42) {
+				$("#move" + target).animate({
+					top: "" + (i * (100 / rows)) + "%",
+					left: "" + (j * (100 / rows)) + "%",
+				});
+			}
+		}
 	}
-	if(puzzle == puzzles){
+}
+
+function next() {
+	if (puzzle !== puzzles) {
+		puzzle = puzzle + 1;
+		locations = $.parseJSON(raw)[puzzle];
+		draw();
+		stop = false;
+		$(".WIN").css("display", "none");
+	}
+	if (puzzle == puzzles) {
 		$(".next").css("height", "3%");
-	}else{
+	} else {
 		$(".next").css("height", "6%");
 	}
-	if(puzzle == 0){
+	if (puzzle == 0) {
 		$(".last").css("height", "3%");
-	}else{
+	} else {
 		$(".last").css("height", "6%");
 	}
 }
-function last(){
-	if(puzzle !== 0){
-	puzzle = puzzle - 1;
-	locations = $.parseJSON(raw)[puzzle];
-	draw();
-	stop = false;
-	$(".WIN").css("display", "none");
+
+function last() {
+	if (puzzle !== 0) {
+		puzzle = puzzle - 1;
+		locations = $.parseJSON(raw)[puzzle];
+		draw();
+		stop = false;
+		$(".WIN").css("display", "none");
 	}
-	if(puzzle == 0){
+	if (puzzle == 0) {
 		$(".last").css("height", "3%");
-	}else{
+	} else {
 		$(".last").css("height", "6%");
 	}
-	if(puzzle == puzzles){
+	if (puzzle == puzzles) {
 		$(".next").css("height", "3%");
-	}else{
+	} else {
 		$(".next").css("height", "6%");
 	}
 }
-function move(index)
-{
-if(!stop){
-var blank;	
-for( var i = 0, len = rows; i < len; i++ ) {
-	for( var j = 0, len = rows; j < len; j++ ) {
-    if( locations[i][j] === 42 ) {
-        bx = i + 1;
-        by = j + 1;
-        break;
-    }
-}	
-}
-var clicked;	
-for( var i = 0, len = rows; i < len; i++ ) {
-	for( var j = 0, len = rows; j < len; j++ ) {
-    if( locations[i][j] === index ) {
-        cx = i + 1;
-        cy = j + 1;
-        break;
-    }
-}	
-}
-if(by == cy + 1 && bx == cx){
-	$( "#move" + index ).animate({
-	left: "+=" + String(100 / rows) +"%",
-	});
-	locations[bx - 1][by - 1] = index;
-	locations[cx - 1][cy - 1] = 42;
-}
-	if(by == cy - 1 && bx == cx){
-	$( "#move" + index ).animate({
-	left: "-=" + String(100 / rows) +"%",
-	});
-	locations[bx - 1][by - 1] = index;
-	locations[cx - 1][cy - 1] = 42;
-}
-	if(by == cy && bx == cx + 1){
-	$( "#move" + index ).animate({
-	top: "+=" + String(100 / rows) +"%",
-	});
-	locations[bx - 1][by - 1] = index;
-	locations[cx - 1][cy - 1] = 42;
-}
-if(by == cy && bx == cx - 1){
-	$( "#move" + index ).animate({
-	top: "-=" + String(100 / rows) +"%",
-	});
-	locations[bx - 1][by - 1] = index;
-	locations[cx - 1][cy - 1] = 42;
-}
-if (String(locations) === win){
-	setTimeout(function(){$(".WIN").css("display", "block");}, 500);
-	stop = true;
-	
-}
-}
+
+function move(index) {
+	if (!stop) {
+		var blank;
+		for (var i = 0, len = rows; i < len; i++) {
+			for (var j = 0, len = rows; j < len; j++) {
+				if (locations[i][j] === 42) {
+					bx = i + 1;
+					by = j + 1;
+					break;
+				}
+			}
+		}
+		var clicked;
+		for (var i = 0, len = rows; i < len; i++) {
+			for (var j = 0, len = rows; j < len; j++) {
+				if (locations[i][j] === index) {
+					cx = i + 1;
+					cy = j + 1;
+					break;
+				}
+			}
+		}
+		if (by == cy + 1 && bx == cx) {
+			$("#move" + index).animate({
+				left: "+=" + String(100 / rows) + "%",
+			});
+			locations[bx - 1][by - 1] = index;
+			locations[cx - 1][cy - 1] = 42;
+		}
+		if (by == cy - 1 && bx == cx) {
+			$("#move" + index).animate({
+				left: "-=" + String(100 / rows) + "%",
+			});
+			locations[bx - 1][by - 1] = index;
+			locations[cx - 1][cy - 1] = 42;
+		}
+		if (by == cy && bx == cx + 1) {
+			$("#move" + index).animate({
+				top: "+=" + String(100 / rows) + "%",
+			});
+			locations[bx - 1][by - 1] = index;
+			locations[cx - 1][cy - 1] = 42;
+		}
+		if (by == cy && bx == cx - 1) {
+			$("#move" + index).animate({
+				top: "-=" + String(100 / rows) + "%",
+			});
+			locations[bx - 1][by - 1] = index;
+			locations[cx - 1][cy - 1] = 42;
+		}
+		if (String(locations) === win) {
+			setTimeout(function () {
+				$(".WIN").css("display", "block");
+			}, 500);
+			stop = true;
+
+		}
+	}
 }
 draw();
-function draw(){
-$(".square").css("background-size", (rows * 100) + "% " + (rows * 100) + "%")
-$(".square").css("width", (100 / rows) +"%")
-$(".square").css("height", (100 / rows) +"%")
-for( var i = 0, len = rows; i < len; i++ ) {
-for( var j = 0, len = rows; j < len; j++ ) {
-	var target = locations[i][j];
-	if(target !== 42){
-	$( "#move" + target ).animate({
-	top: "" + (i *  (100 / rows)) + "%",
-	left: "" + (j * (100 / rows)) + "%",
-});
-console.log(i + j);
-var tx = (target - 1) % rows;
-var ty = Math.floor((target - 1) / rows);
-var multiple = 100 / rows
 
-$("#move" + target).css("background-position", ((tx * multiple) + 8 * tx) + "% " + ((ty * multiple) + 8 * ty)+ "%");
+function draw() {
+	$(".square").css("background-size", (rows * 100) + "% " + (rows * 100) + "%")
+	$(".square").css("width", (100 / rows) + "%")
+	$(".square").css("height", (100 / rows) + "%")
+	for (var i = 0, len = rows; i < len; i++) {
+		for (var j = 0, len = rows; j < len; j++) {
+			var target = locations[i][j];
+			if (target !== 42) {
+				$("#move" + target).animate({
+					top: "" + (i * (100 / rows)) + "%",
+					left: "" + (j * (100 / rows)) + "%",
+				});
+				console.log(i + j);
+				var tx = (target - 1) % rows;
+				var ty = Math.floor((target - 1) / rows);
+				var multiple = 100 / rows
+
+				$("#move" + target).css("background-position", ((tx * multiple) + 8 * tx) + "% " + ((ty * multiple) + 8 * ty) + "%");
+			}
+		}
+	}
 }
-}
-}
-}
-$(".last").css("height","3px");
+$(".last").css("height", "3px");
